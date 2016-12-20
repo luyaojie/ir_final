1605dcb054a4f44bdc96e24d6dfd2e803db4a08b
Roger <luyaojie1993@gmail.com>
2016-12-20 21:06:36 +0800

    Preprocessing - Extract URL

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 # Preprocessing
 
-# TODO
-# URL Dect
-# http://www. -> None
-# http:// -> None
-# https://
 
-
-def replace_non_chinese(input_filename, output_filename, remove_num=True, remove_alpha=True):
+def replace_non_chinese(input_filename, output_filename, remove_num=True, remove_alpha=True, remove_url=True):
     """
     replace number in query -- file level
     :param input_filename:
     :param output_filename:
     :param remove_num:
-    :param remove_english:
+    :param remove_alpha:
+    :param remove_url:
     :return:
     """
     output = open(output_filename, 'w')
@@ -25,6 +20,8 @@ def replace_non_chinese(input_filename, output_filename, remove_num=True, remove
                 line = replaceNumByLine(line)
             if remove_alpha:
                 line = replaceEnglishByLine(line)
+            if remove_url:
+                line = replaceUrlByLine(line)
             output.write("%s\n" % line)
     output.close()
 
@@ -39,7 +36,7 @@ def replaceNumByLine(line, replace=""):
     att = line.split("\t")
     label = att[:4]
     queries = att[4:]
-    return "\t".join(label + [replaaceNumInQuery(q, replace) for q in queries])
+    return "\t".join(label + [replaceNumInQuery(q, replace) for q in queries])
 
 
 def replaceEnglishByLine(line, replace=""):
@@ -52,10 +49,23 @@ def replaceEnglishByLine(line, replace=""):
     att = line.split("\t")
     label = att[:4]
     queries = att[4:]
-    return "\t".join(label + [replaaceEnglishInQuery(q, replace) for q in queries])
+    return "\t".join(label + [replaceEnglishInQuery(q, replace) for q in queries])
 
 
-def replaaceNumInQuery(query, replace=""):
+def replaceUrlByLine(line, extract=True):
+    """
+    replace English in query —— line level
+    :param line:
+    :param extract:
+    :return:
+    """
+    att = line.split("\t")
+    label = att[:4]
+    queries = att[4:]
+    return "\t".join(label + [replaceUrlInQuery(q, extract) for q in queries])
+
+
+def replaceNumInQuery(query, replace=""):
     """
     replace Number in query —— line level
     :param query:
@@ -65,7 +75,7 @@ def replaaceNumInQuery(query, replace=""):
     return " ".join([word if not word.isdigit() else replace for word in query.split()]).strip()
 
 
-def replaaceEnglishInQuery(query, replace=""):
+def replaceEnglishInQuery(query, replace=""):
     """
     replace English Word in query —— line level
     :param query:
@@ -73,6 +83,48 @@ def replaaceEnglishInQuery(query, replace=""):
     :return:
     """
     return " ".join([word if not word.isalpha() else replace for word in query.split()]).strip()
+
+
+def replaceUrlInQuery(query, extract=True):
+    """
+    extract url in query
+    :param query:
+    :param extract:
+    :return:
+    """
+    if is_url(query) and extract:
+        return extract_url_keyword(query)
+    return query
+
+
+def is_url(query):
+    """
+    judge a query whether url or not
+    :param query:
+    :return:
+    """
+    if query.startswith("http") or query.startswith("www"):
+        return True
+    else:
+        return False
+
+
+def extract_url_keyword(url):
+    """
+    Extract Url Keyword
+    :param url:
+    :return:
+    """
+    url = url.replace(" ", "")
+    if "http://" in url:
+        url = url.replace("http://", "")
+    if "https://" in url:
+        url = url.replace("https://", "")
+    if "www." in url:
+        url = url.split("www.")[1]
+    if "/" in url:
+        url = url.split("/")[0]
+    return url
 
 
 if __name__ == "__main__":
@@ -83,8 +135,10 @@ if __name__ == "__main__":
     parser.add_argument('-o', '--output', dest='output_file', type=str, help='Output File')
     parser.add_argument('--keep-num', dest='remove_num', action='store_false', help='Keep Number\tDefalut Remove')
     parser.add_argument('--keep-alpha', dest='remove_alpha', action='store_false', help='Keep Alpha\tDefalut Remove')
+    parser.add_argument('--keep-url', dest='remove_url', action='store_false', help='Keep URL\tDefalut Remove')
     parser.set_defaults(remove_num=True)
     parser.set_defaults(remove_alpha=True)
+    parser.set_defaults(remove_url=True)
     args = parser.parse_args()
     replace_non_chinese(input_filename=args.input_file,
                         output_filename=args.output_file,
